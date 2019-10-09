a4a20cd57541d5c1d4d80e69fce7856dabd1fcf1
SSypp <2435884205@qq.com>
2019-10-09 11:53:38 +0800

    '生物多样性'

--- src/pages/bioddetailsPage/components/biod_list_details.vue
+++ src/pages/bioddetailsPage/components/biod_list_details.vue
@@ -1,8 +1,8 @@
 <template lang='pug'>
     div(class='biod_list_box')
         div(class='biod_list_title')
-            span(class='title-CN') 鸟类 
-            span(class='title-EN') Aves
+            span(class='title-CN' v-text="title")  
+            span(class='title-EN' v-text="e_title")
         
         div(class='biod_list_img')
             div(class='list_img' 
@@ -10,12 +10,12 @@
             v-on:mouseenter="enter(index)"
             v-on:mouseleave="leave()"
             )
-                img(:src="item.imgURL")
+                img(:src="item.thumbnail")
                 div(v-show="one_shade && index == current" class="list_img_shade")
-                    span(class="titleText") {{item.imgTitle}}
-                    span(class="img_desc") {{item.imgMsg}}
+                    span(class="titleText") {{item.name}}
+                    span(class="img_desc") {{item.brief}}
                     span(class="t")
-                    a(class='btn' href='/bioddetails.html' ) {{btn_msg}}
+                    a(class='btn' @click="getDetailsPage(item)") {{btn_msg}}
                     
 </template>
 <script>
@@ -26,8 +26,11 @@ export default {
   data() {
     return {
       btn_msg: "查看详情",
+      title:'',
+      e_title:'',
       one_shade: true,
       current: null,
+      currentId: null,
       list_details: [
         {
           id: 1,
@@ -130,19 +133,63 @@ export default {
       let r = window.location.search.substr(1).match(reg);
       if (r != null) return decodeURIComponent(r[2]);
       return null;
+    },
+    getIdPages(){
+      var url = location.search;
+      if (url.indexOf("?") != -1) {    //判断是否有参数
+        var str = url.substr(1);       //从第一个字符开始 因为第0个是?号 获取所有除问号的所有符串
+        //console.log(str);
+        var strs = str.split("=");   //用等号进行分隔 （因为知道只有一个参数 所以直接用等号进分隔 如果有多个参数 要用&号分隔 再用等号进行分隔）
+        this.currentId = strs[1];          //直接弹出第一个参数 （如果有多个参数 还要进行循环的）
+      }
+      if(this.currentId == 2){
+        this.title = "鸟类";
+        this.e_title = "Aves";
+      }
+      if(this.currentId == 3){
+        this.title = "鱼类";
+        this.e_title = "Fish";
+      }
+      if(this.currentId == 4){
+        this.title = "两栖类及爬行类";
+        this.e_title = "Amphibians And Reptiles";
+      }
+      if(this.currentId == 5){
+        this.title = "无脊椎动物";
+        this.e_title = "Invertebrate";
+      }
+      if(this.currentId == 6){
+        this.title = "哺乳类";
+        this.e_title = "Mammalia";
+      }
+      if(this.currentId == 8){
+        this.title = "植物与真菌";
+        this.e_title = "Plants And Fungi";
+      }
+      this.$_get(API.BIODIVERSITY_SPECIESID+this.currentId).then(res => {
+        if(!res.data.isError){
+          this.list_details = res.data;
+        }
+        console.log(res.data)
+    })
+    },
+    getDetailsPage(item){
+      location.href= `/bioddetails.html?id=${item.id}`
     }
+
   },
   created() {
-    if (this.getQueryVariable("id")) {
-      this.$_get(API.BIODIVERSITY_DETAIL_LIST_DATA, {
-        speciesId: this.getQueryVariable("id")
-      }).then(res => {
-        console.log(res)
-        if (!res.data.isError) {
-          this.kindList = res.data;
-        }
-      });
-    }
+    this.getIdPages();
+    // if (this.getQueryVariable("id")) {
+    //   this.$_get(API.BIODIVERSITY_DETAIL_LIST_DATA, {
+    //     speciesId: this.getQueryVariable("id")
+    //   }).then(res => {
+    //     console.log(res)
+    //     if (!res.data.isError) {
+    //       this.kindList = res.data;
+    //     }
+    //   });
+    // }
   }
 };
 </script>
@@ -154,7 +201,7 @@ export default {
   margin: 0 auto;
 }
 .biod_list_title {
-  width: 172px;
+  width: auto;
   height: 48px;
 }
 .title-CN {
@@ -162,6 +209,8 @@ export default {
   font-size: 34px;
   color: rgb(45, 47, 41);
   letter-spacing: 2px;
+  margin-right:10px;
+  line-height:48px;
 }
 .title-EN {
   font-family: CenturyGothic;
