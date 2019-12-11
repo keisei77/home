ae9611646cc32729bb5c7e860dee3cb9eeab0ba7
foosirR <foosir@163.com>
2019-12-11 09:08:19 +0800

    修复感谢摄影师行高，去除测试信息

--- src/pages/indexPage/indexcomponents/partners.vue
+++ src/pages/indexPage/indexcomponents/partners.vue
@@ -14,7 +14,7 @@
 			div(class='partners_title_box')
 				div(class="CH_title") 支持单位
 					p(class="EN_title")
-						i SUPPORT test
+						i SUPPORT
 		div(class='partners_group')
 			div(class='partners_list' v-for='partners of partners_data')
 				a(:href='partners.link')
@@ -27,99 +27,103 @@
 	footer-tab
 </template>
 <script>
-import FooterTab from '../../components/footer_tab.vue'
+import FooterTab from "../../components/footer_tab.vue";
 export default {
-	name: 'partner',
-	props: ['partners_data','organizer','photographer'],
-	components:{
-		FooterTab
-	}
-}
+  name: "partner",
+  props: ["partners_data", "organizer", "photographer"],
+  components: {
+    FooterTab
+  }
+};
 </script>
 <style lang="scss" scoped>
-	.partners_box{ 
-		width: 100%;
-		/*height: calc(100vh - 90px);*/
-    padding-top: 70px;
-    box-sizing: border-box;
-	}
-	.partners_title{
-		width: 100%;
-		height: 146px;
-	}
-	.partners_title_box{
-		width: 1000px;
-		position: relative;
-		height: 146px;
-		margin: 0 auto;
-	}
-	.CH_title{
-		position: absolute;
-		width: 100%;
-		height: 56px;
-		text-align: center;
-		line-height: 56px;
-		font-family: PingFangSC-Semibold;
-		font-size: 40px;
-		color: #2D2F29;
-	}
-	.EN_title{
-		width: 100%;
-		height: 43px;
-		text-align: center;
-		line-height: 43px;
-		opacity: 0.93;
-		font-family: GillSans-SemiBoldItalic;
-		font-size: 36px;
-		color: #46787D;
-		letter-spacing: 0.6px;
-	}
-	.partners_group{
-		width: 1200px;
-		margin: 0 auto;
-		display: flex;
-		flex-wrap: wrap;
-    justify-content: center;
-		overflow: hidden;
-		position: relative;
-	}
-	.partners_list{
-		height: 118px;
-		margin-bottom: 30px;
-    & img{
-      max-width: 225px;
-      max-height: 75px;
-    }
-	}
-	.partners_list{
-		margin-right: 47px; 
-	}
-	.photographer_list_title{
-		line-height:250%;font-weight: bold;text-align:center;}
-	.photographer_list_memo{
-		line-height:250%;text-align:center;}
-	.photographer_list{
-		line-height:250%;
-		font-family: PingFangSC-Semibold;
-		text-align: center;
-		font-size: 18px;
-		max-width: 1100px;
-		margin: 0 auto;
-		margin-top: -20px;
-		color: #2d2f29;
-		span{
-			margin-left: 15px;
-
-		}
-	}
-	/*.partners_list:nth-child(3){*/
-		/*position:absolute;*/
-		/*top: 0;*/
-		/*right:0;*/
-	/*}*/
-	/*.partners_list:nth-child(6) {*/
-		/*position:absolute;*/
-		/*top: 158px;*/
-		/*right:0;*/
-	 /*}*/
+.partners_box {
+  width: 100%;
+  /*height: calc(100vh - 90px);*/
+  padding-top: 70px;
+  box-sizing: border-box;
+}
+.partners_title {
+  width: 100%;
+  height: 146px;
+}
+.partners_title_box {
+  width: 1000px;
+  position: relative;
+  height: 146px;
+  margin: 0 auto;
+}
+.CH_title {
+  position: absolute;
+  width: 100%;
+  height: 56px;
+  text-align: center;
+  line-height: 56px;
+  font-family: PingFangSC-Semibold;
+  font-size: 40px;
+  color: #2d2f29;
+}
+.EN_title {
+  width: 100%;
+  height: 43px;
+  text-align: center;
+  line-height: 43px;
+  opacity: 0.93;
+  font-family: GillSans-SemiBoldItalic;
+  font-size: 36px;
+  color: #46787d;
+  letter-spacing: 0.6px;
+}
+.partners_group {
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+}
+.partners_list {
+  height: 118px;
+  margin-bottom: 30px;
+  & img {
+    max-width: 225px;
+    max-height: 75px;
+  }
+}
+.partners_list {
+  margin-right: 47px;
+}
+.photographer_list_title {
+  line-height: 250%;
+  font-weight: bold;
+  text-align: center;
+}
+.photographer_list_memo {
+  line-height: 250%;
+  text-align: center;
+}
+.photographer_list {
+  line-height: 250%;
+  font-family: PingFangSC-Semibold;
+  text-align: center;
+  font-size: 18px;
+  max-width: 1100px;
+  margin: 0 auto;
+  /*margin-top: -20px;*/
+  color: #2d2f29;
+  span {
+    margin-left: 15px;
+  }
+}
+/*.partners_list:nth-child(3){*/
+/*position:absolute;*/
+/*top: 0;*/
+/*right:0;*/
+/*}*/
+/*.partners_list:nth-child(6) {*/
+/*position:absolute;*/
+/*top: 158px;*/
+/*right:0;*/
+/*}*/
 </style>
