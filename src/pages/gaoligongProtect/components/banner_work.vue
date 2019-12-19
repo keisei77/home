23ef4c8c8516510295e3b08d67c49d7bccf904c3
pingping <365342727@qq.com>
2019-12-19 15:23:53 +0800

    死数据抽离到index.js

--- src/pages/gaoligongProtect/components/banner_work.vue
+++ src/pages/gaoligongProtect/components/banner_work.vue
@@ -6,40 +6,17 @@
             img(class='banner_work_img' :src="require('../../../images/' + v.banner_work_img)")
 </template>
 <script>
+import { s_data, b_data, r_data } from "./index.js";
 export default {
   name: "banner_work",
   data() {
     return {
-      s_bannerList: [
-        {
-          banner_work_title: "半常绿季雨林",
-          banner_work_msg:
-            "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
-          banner_work_img: "s1.png"
-        },
-        {
-          banner_work_title: "河谷稀树灌木草丛",
-          banner_work_msg:
-            "处于炎热、季节性干旱气候条件，具有明显的次生性质，外貌上以草丛为主，其间散生低矮的灌木和乔木。",
-          banner_work_img: "s2.png"
-        }
-      ],
-      b_bannerList: [
-        {
-          banner_work_title: "半常绿季雨林",
-          banner_work_msg:
-            "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
-          banner_work_img: "s2.png"
-        }
-      ],
-      r_bannerList: [
-        {
-          banner_work_title: "半常绿季雨林",
-          banner_work_msg:
-            "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
-          banner_work_img: "s2.png"
-        }
-      ]
+      //生态系统数据
+      s_bannerList: s_data,
+      //保护工作数据
+      b_bannerList: b_data,
+      //历史人文数据
+      r_bannerList: r_data
     };
   },
   mounted() {},
---
+++ src/pages/gaoligongProtect/components/index.js
@@ -0,0 +1,34 @@
+export let s_data = [{
+        banner_work_title: "半常绿季雨林",
+        banner_work_msg: "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
+        banner_work_img: "s1.png"
+    },
+    {
+        banner_work_title: "河谷稀树灌木草丛",
+        banner_work_msg: "处于炎热、季节性干旱气候条件，具有明显的次生性质，外貌上以草丛为主，其间散生低矮的灌木和乔木。",
+        banner_work_img: "s2.png"
+    }
+]
+export let b_data = [{
+        banner_work_title: "保护管理",
+        banner_work_msg: "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
+        banner_work_img: "s1.png"
+    },
+    {
+        banner_work_title: "保护管理",
+        banner_work_msg: "处于炎热、季节性干旱气候条件，具有明显的次生性质，外貌上以草丛为主，其间散生低矮的灌木和乔木。",
+        banner_work_img: "s2.png"
+    }
+]
+
+export let r_data = [{
+        banner_work_title: "云端上的战争",
+        banner_work_msg: "两层的封闭的乔木层，降水充沛，干湿季分明，常绿乔木与雨林植物",
+        banner_work_img: "s1.png"
+    },
+    {
+        banner_work_title: "南方丝绸古道",
+        banner_work_msg: "处于炎热、季节性干旱气候条件，具有明显的次生性质，外貌上以草丛为主，其间散生低矮的灌木和乔木。",
+        banner_work_img: "s2.png"
+    }
+]
