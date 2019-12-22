a0e42c5960741ee55e6a943f9521ecd312914ee7
SSypp <2435884205@qq.com>
2019-12-22 11:45:18 +0800

    更改明星物种详情页

--- src/js/speciesDetail.js
+++ src/js/speciesDetail.js
@@ -1,6 +1,6 @@
 import App from "../../app";
+import "../scss/speciesdetail.scss";
 import common from "./common/common";
-import "../scss/speciesDetail.scss";
 import Speciesdetailpage from "../pages/speciesDetail/page_speciesdetail.vue";
 
 new App({
