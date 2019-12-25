f8e8c3fd4219fc8fc28addfb08ec3b98dd539092
foosirR <foosir@163.com>
2019-12-25 12:48:14 +0800

    大小写命名测试

---
+++ src/js/speciesdetail_temp.js
@@ -0,0 +1,8 @@
+import App from "../../app";
+import "../scss/speciesdetail.scss";
+import common from "./common/common";
+import Speciesdetailpage from "../pages/speciesDetail/page_speciesdetail.vue";
+
+new App({
+  Speciesdetailpage
+});
